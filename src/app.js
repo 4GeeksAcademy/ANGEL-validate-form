83497ec2d59f5a3d893f6ce69c36a463b597fd9e
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-08-07 22:53:25 +0000

    First advance

--- src/app.js
+++ src/app.js
@@ -5,7 +5,73 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.addEventListener("DOMContentLoaded", () => {
+  let button = document.getElementById("submit");
+  let missingDiv = document.getElementById("missing-div");
+  missingDiv.style.display = "none";
+
+  button.addEventListener("click", event => {
+    event.preventDefault();
+
+    let inputs = document.querySelectorAll("input, textarea");
+
+    let isValid = true;
+    inputs.forEach(input => {
+      input.classList.remove("input-error");
+
+      if (input.id === "cardNumber") {
+        if (!/^\d{16}$/.test(input.value)) {
+          isValid = false;
+          //missingDiv.innerHTML = "Numero de tarjeta invalido";
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "cvcNumber") {
+        if (!/^\d{3}$/.test(input.value)) {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "nameId") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.type === "textarea") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "amount") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "cityId") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "lastnameId") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "stateId") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      } else if (input.id === "postalCodeId") {
+        if (input.value.trim() === "") {
+          isValid = false;
+          input.classList.add("input-error");
+        }
+      }
+    });
+
+    if (!isValid) {
+      missingDiv.style.display = "block";
+    } else {
+      missingDiv.style.display = "none";
+    }
+  });
+});
